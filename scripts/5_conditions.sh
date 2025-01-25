808b5bc556c1fbe8956775165c8384cd2cf22f59
Lal Castro <lal.castro@icloud.com>
2025-01-25 21:05:46 +0530

    conditions

---
+++ scripts/5_conditions.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+echo "Conditions"
+
+if [[ 1 -eq 1 ]]; then
+    echo "1 is equal to 1"
+fi
+
+read -p "How old are you? " number
+if [[ $number -gt 50 ]]; then
+    echo "You are not very young"
+else
+    echo "You are young"
+fi
+
+read -p "type an integer between 1 and 4": num
+if [[ $num -eq 1 ]]; then
+    echo "You typed 1"
+elif [[ $num -eq 2 ]]; then
+    echo "You typed 2"
+elif [[ $num -eq 3 ]]; then
+    echo "You typed 3"
+elif [[ $num -eq 4 ]]; then
+    echo "You typed 4"
+else
+    echo "You typed an invalid number"
+fi
+
+
+# 3 -eq 3  		3 = 3
+# 3 -ne 4		3 is not 4
+# 3 -gt 1		3 > 1
+# 3 -lt 7		3 < 7
+
+# 3 -ge 3 		3 >= 3
+# 3 -le 3 		3 <= 3
