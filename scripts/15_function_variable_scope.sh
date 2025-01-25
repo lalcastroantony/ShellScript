991f950e3022b254cf0f97a86f287fcaa5f68062
Lal Castro <lal.castro@icloud.com>
2025-01-25 23:14:53 +0530

    variable scope

---
+++ scripts/15_function_variable_scope.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+
+myfunction(){
+
+	local var1="Eggs"
+		var2="Salad"
+
+	echo "my variable inside the funct is: $var1"
+}
+
+
+echo "start the program"
+
+myfunction
+
+echo "Outside :  $var1"
+echo "Outside :  $var2"
